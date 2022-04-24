b3a10e93007fb54db6589afea63d39b1078c7e56
Majbar Yassine <ymajbar.pro@gmail.com>
2022-04-24 23:29:04 +0000

    p3

--- src/main/java/ma/enset/etudiantmvc/security/repositories/AppUserReository.java
+++
@@ -1,8 +0,0 @@
-package ma.enset.etudiantmvc.security.repositories;
-
-import ma.enset.etudiantmvc.security.entities.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserReository extends JpaRepository<AppUser,String> {
-    AppUser findByUsername(String username);
-}
